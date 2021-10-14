14c0c7679b12dbffd828e969156fa3b337484022
coolAvi007 <92530622+coolAvi007@users.noreply.github.com>
2021-10-14 19:41:55 +0530

    Update messages.js

--- nav/messages.js
+++ nav/messages.js
@@ -4,7 +4,7 @@ import FontAwesome, { Icons } from "react-native-fontawesome";
 import {Header} from 'react-native-elements';
 export default class third extends React.Component{
   static navigationOptions={
-    tabBarLabel: 'DM'
+    tabBarLabel: 'Inbox'
     }
     render(){
       return (
@@ -12,7 +12,8 @@ export default class third extends React.Component{
       {
       flex: 1,
       justifyContent: 'center',
-      alignItems: 'center'
+      alignItems: 'center',
+        wrapText:true
       }
     }>
 
@@ -20,8 +21,8 @@ export default class third extends React.Component{
     <Image 
 style={{width: 120, height: 120, marginTop:30, marginBottom:30, alignItems: 'center'}}
 source={{uri:'https://pbs.twimg.com/profile_images/875603699694948354/vR77CGM9_400x400.jpg'}}/>
-    <Text style={{fontSize: 30, color: '#55ACEE'}}>
-    All Direct Messages are found here
+    <Text style={{fontSize: 20, color: '#55BCEE'}}>
+    Direct Message Component
     </Text>
         </View>  
    ); }}
