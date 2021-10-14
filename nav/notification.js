93e9104ab83b4e9e44dc3a44a6ae4a4df0c41e3b
GIPhoenix <92399947+GIPhoenix@users.noreply.github.com>
2021-10-14 19:12:17 +0530

    Update notification.js

--- nav/notification.js
+++ nav/notification.js
@@ -11,12 +11,13 @@ export default class third extends React.Component{
       {
       flex: 1,
       justifyContent: 'center',
-      alignItems: 'center'
+      alignItems: 'center',
+        wrapText:true
       }
     }>
 
     <Image 
-style={{width: 120, height: 120, marginTop:30, marginBottom:30, alignItems: 'center'}}
+style={{width: 160, height: 180, marginTop:20, marginBottom:30, alignItems: 'center'}}
 source={{uri:'https://pbs.twimg.com/profile_images/875603699694948354/vR77CGM9_400x400.jpg'}}/>
     <Text style={{fontSize: 30, color: '#55ACEE'}}>
     Notification Area
