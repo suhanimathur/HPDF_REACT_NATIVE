8fb0eed0c5348794d25b6ec9743190311650c0c1
sachinhrx <92531840+sachinhrx@users.noreply.github.com>
2021-10-14 19:56:13 +0530

    Update home.js

--- nav/home.js
+++ nav/home.js
@@ -18,14 +18,15 @@ export default class third extends React.Component{
       {
       flex: 1,
       justifyContent: 'center',
-      alignItems: 'center'
+      alignItems: 'center',
+       wrapText:true
       }
     }>
     <Image 
 style={{width: 120, height: 120, marginTop:30, marginBottom:30, alignItems: 'center'}}
 source={{uri:'https://pbs.twimg.com/profile_images/875603699694948354/vR77CGM9_400x400.jpg'}}/>
     <Text style={{fontSize: 30, color: '#55ACEE'}}>
-    Your Tweet Feed
+    Your Twitter feed. Please Tweet.
     </Text>
         </View>  
     }}
